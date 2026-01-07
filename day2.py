17bebda893a63f53f7dc61d8ac89b9a73ec7242c
MADHULIKA MATTIPALLI <118009876+emandemtech@users.noreply.github.com>
2026-01-07 17:05:06 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,17 @@
+#datatypes, variables and i/o functions
+number = 2
+decimal_value = 2.5
+name = "Madhu"
+age = 24
+lst = [1,2,'hi','20']
+tp = (1,2,3,4)
+st = {1,2,3,4}
+details = {'batch': 47}
+print(number)
+print(decimal_value)
+print("My name is:",name)
+print("My age is:",age)
+print(lst)
+print(tp)
+print(st)
+print(details)
