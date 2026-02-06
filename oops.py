35a0a8c6e5ea517831aafc9a9a9ee3b98b904df7
Madhulika Mattipalli <madhulikamattipalli1305@gmail.com>
2026-02-06 16:45:03 +0530

    updates

--- oops.py
+++ oops.py
@@ -1,3 +1,29 @@
+# class User:
+#     def __init__(self, name, mail, password):
+#         self.name = name
+#         self.mail = mail
+#         self.password = password
+#     def details(self):
+#         return " name is, {}  mail is, {} password is, {}".format(self.name, self.mail, self.password)
+    
+# class Admin(User):
+#     pass
+#     # def __init__(self, name, mail, password, total_income, total_user_count):
+#     #     self.name = name
+#     #     self.mail = mail
+#     #     self.password = password
+#     #     self.total_income = total_income
+#     #     self.total_users = total_user_count
+#     # def details(self):
+#     #     return " name is, {} mail is, {} password is, {} total_income is {}, total_users{}".format(self.name, self.mail, self.password, self.total_income, self.total_users)
+    
+# u = User("Ravi", "ravi@gmail.com", "1234")
+# a = Admin("Admin1", "admin@gmail.com", "admin123") #50000, 120
+
+# print(u.details())
+# print(a.details())
+
+#parent class with implementation:
 class User:
     def __init__(self, name, mail, password):
         self.name = name
@@ -6,7 +32,7 @@ class User:
     def details(self):
         return " name is, {}  mail is, {} password is, {}".format(self.name, self.mail, self.password)
     
-class Admin:
+class Admin(User):
     def __init__(self, name, mail, password, total_income, total_user_count):
         self.name = name
         self.mail = mail
@@ -14,10 +40,10 @@ class Admin:
         self.total_income = total_income
         self.total_users = total_user_count
     def details(self):
-        return " name is, {} mail is, {} password is, {} total_income is {}, total_users{}".format(self.name, self.mail, self.password, self.total_income, self.total_users)
-    
+        return " name is, {}  mail is, {} password is, {} total_income is {}, total_users is {}".format(self.name, self.mail, self.password, self.total_income, self.total_users)
 u = User("Ravi", "ravi@gmail.com", "1234")
 a = Admin("Admin1", "admin@gmail.com", "admin123", 50000, 120)
 
 print(u.details())
 print(a.details())
+
